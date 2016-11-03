c5604cc5719a44dae7ed0d54262e201938f4b9e6
Ian Patton <ian.patton@gmail.com>
2016-11-03 15:29:29 -0400

    make more configurable

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const util = require('util');
 const winston = require('winston');
 
 const format = (options) => {
-  const timestamp = new Date();
+  let timestamp = '';
+  let level = '';
+  if (options.showTimestamps) timestamp = `${new Date().toISOString()} `;
+  if (options.showLevel) level = `${options.level.toUpperCase()}\t`;
   let outputString = options.message;
   if (options.meta && Object.keys(options.meta).length) {
     outputString += `\t${JSON.stringify(options.meta)}`;
   }
-  return `${timestamp.toISOString()} ${options.level.toUpperCase()} ${outputString}`;
+  return `${timestamp}${level}${outputString}`;
 };
 
 const Logger = function (config) {
   config.log = config.log || {};
-
   const logger = new winston.Logger({
     transports: [
       // we can add additional transports here to log to multiple destinations
       new winston.transports.Console({
         formatter: format,
-        level: config.log.level || 'debug'
+        level: config.log.level || 'debug',
+        showTimestamps: config.log.showTimestamps || true,
+        showLevel: config.log.showLevel || true
       })
     ]
   });
